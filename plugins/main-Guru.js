05fae75d048728b9c2713533a86ccc1fce13d2db
hghardy2 <89137410+hghardy2@users.noreply.github.com>
2024-02-11 20:36:46 +0100

    unzipgit

---
+++ plugins/main-Guru.js
@@ -0,0 +1,33 @@
+let handler = async m => m.reply(`
+
+≡  *HARDY  ┃ ᴮᴼᵀ*   GROUPS
+
+─────────────
+▢ Join public bot group and support
+https://chat.whatsapp.com/DSY6fotLMdO7qRH00H7otQ
+
+▢ Group 2
+https://chat.whatsapp.com/DSY6fotLMdO7qRH00H7otQ
+
+▢ Group 3
+https://chat.whatsapp.com/Jn9qVerYN6Q1L0vlVFe2XL
+
+─────────────
+≡ Disabled links? enter here! 
+
+▢ Group WhatsApp 
+ https://chat.whatsapp.com/Bar4iEFh0IIIs4C2k0QSWG
+─────────────
+▢ *Owner Facebook*
+ https://www.facebook.com/gxaymenofc
+
+▢ *Instagram*
+• https://www.instagram.com/@gxaymenofc
+
+
+`.trim())
+handler.help = ['gphardy']
+handler.tags = ['main']
+handler.command = ['groups', 'grouphardy', 'hggp', 'hgp', 'gphardy'] 
+
+export default handler
